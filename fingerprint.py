6b0ae064c19965c422482a91dce4e6721e737f2f
无星夜 <31583810+TrumanH@users.noreply.github.com>
2020-01-09 13:45:37 +0800

    Create fingerprint.py

---
+++ fingerprint.py
@@ -0,0 +1,74 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2019/1/9 10:52
+# @Author  : Truman He
+
+import hashlib 
+import weakref
+
+fingerprint_cache = weakref.WeakKeyDictionary()
+
+def request_fingerprint(request, include_headers=None):
+    """
+    Return the request fingerprint.
+
+    The request fingerprint is a hash that uniquely identifies the resource the
+    request points to. For example, take the following two urls:
+
+    http://www.example.com/query?id=111&cat=222
+    http://www.example.com/query?cat=222&id=111
+
+    Even though those are two different URLs both point to the same resource
+    and are equivalent (ie. they should return the same response).
+
+    Another example are cookies used to store session ids. Suppose the
+    following page is only accesible to authenticated users:
+
+    http://www.example.com/members/offers.html
+
+    Lot of sites use a cookie to store the session id, which adds a random
+    component to the HTTP Request and thus should be ignored when calculating
+    the fingerprint.
+
+    For this reason, request headers are ignored by default when calculating
+    the fingeprint. If you want to include specific headers use the
+    include_headers argument, which is a list of Request headers to include.
+
+    """
+    if include_headers:
+        include_headers = tuple(to_bytes(h.lower())
+                                 for h in sorted(include_headers))
+    cache = _fingerprint_cache.setdefault(request, {})
+    if include_headers not in cache:
+        fp = hashlib.sha1()
+        fp.update(to_bytes(request.method))
+        fp.update(to_bytes(canonicalize_url(request.url)))
+        fp.update(request.body or b'')
+        if include_headers:
+            for hdr in include_headers:
+                if hdr in request.headers:
+                    fp.update(hdr)
+                    for v in request.headers.getlist(hdr):
+                        fp.update(v)
+        cache[include_headers] = fp.hexdigest()
+    return cache[include_headers]
+    
+def to_bytes(text, encoding=None, errors='strict'):
+    """Return the binary representation of ``text``. If ``text``
+    is already a bytes object, return it as-is."""
+    if isinstance(text, bytes):
+        return text
+    if not isinstance(text, six.string_types):
+        raise TypeError('to_bytes must receive a unicode, str or bytes '
+                        'object, got %s' % type(text).__name__)
+    if encoding is None:
+        encoding = 'utf-8'
+    return text.encode(encoding, errors)
+
+def to_native_str(text, encoding=None, errors='strict'):
+    """ Return str representation of ``text``
+    (bytes in Python 2.x and unicode in Python 3.x). """
+    if six.PY2:
+        return to_bytes(text, encoding, errors)
+    else:
+        return to_unicode(text, encoding, errors)
+
