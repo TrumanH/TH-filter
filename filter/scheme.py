e0868e553ffdaa543a4527f1f7c882658ffb64fc
TrumanH <914287758@qq.com>
2020-07-26 18:49:33 +0800

    update scheme.

--- filter/scheme.py
+++ filter/scheme.py
@@ -7,12 +7,21 @@
 import os
 import schedule
 # from connection import get_redis, params
+from threading import Thread
 from settings import today, history, Expire_days, DirectoryOfLocalFile
 from filter.bitmap import Bitmap
 from filter import Filter
 
 
 class Scheme():
+    _instance = None
+
+    # 单例模式
+    def __new__(cls, *args, **kwargs):
+        if not cls._instance:
+            cls._instance = object.__new__(cls)
+        return cls._instance
+
     def __init__(self):
         """
             初始化函数,如Expire_days为3天,则：
@@ -29,39 +38,58 @@ class Scheme():
             # ex:过期时间(秒),px:过期时间(毫秒);nx:只在键不存在时才对键进行设置操作，默认false;px:只在键已经存在时才对键进行设置操作，默认false
             self.redis.set(history + "_" + str(i), "", ex=None, px=None, nx=True, xx=False)     # 不存在时才对键进行设置操作
         """
-        schedule.every().day.at("00:00").do(self.transfer)
+        if Expire_days:
+            #schedule.every().day.at("00:00").do(self.transfer)  # productive
+            schedule.every().minute.do(lambda:print("test transfer")) # only for test
+            while True:
+                schedule.run_pending()
 
         self.today_filter = self.bitmap.as_filter(today)
         self.history_filter = self.bitmap.as_filter(history)
 
+    # def cycle_init(self):
+    #     print("a thread to execute cycle job")
+    #     thread = Thread()
+
     def transfer(self):
         """ can be optimize"""
+        print("start transfer...")
         historys = [history + "_" + str(i) for i in list(range(1, Expire_days+1))[::-1]]  # 3,2,1
         # 本地文件命名更新(+1)处理 : history_2, history_3,...
         os.chdir(DirectoryOfLocalFile)  # 到文件储存文件夹里
         for file in historys:
             new_name = "history_" + str(int(file.split("_")[-1])+1)
-            os.rename(file, new_name)
+            try:
+                os.rename(file, new_name)
+            except FileNotFoundError as e:
+                print(e, "The Origin File Not Exist?")
         # today bitmap 存为本地文件 history_1
         self.bitmap.saveBitmap(today, "history_1")
         # 创建一个新的 today bitmap
         self.bitmap.generateBitmap(key="today")
         self.bitmap.generateBitmap(key="history_new")
         for key in historys:  # ...history_3, history_2, history_1
-            self.bitmap.generateBitmapFromLocal(key_name=key, local_file_name=key)
-            self.bitmap.redis.bitop("OR", "history_new", key)
-            self.bitmap.redis.delete(key)
+            ok = self.bitmap.generateBitmapFromLocal(key_name=key, local_file_name=key) # True of False
+            if ok:
+                print("bitop or history_new {}".format(key))
+                self.bitmap.redis.bitop("OR", "history_new", key)
+                self.bitmap.redis.delete(key)
 
         self.bitmap.redis.rename("history_new", history)  # history_new 将原来history 覆盖掉
+        print("done transfer ")
 
-    def insert(self, feature):
+    # 不建议生产使用, only for test
+    def _insert(self, feature):
         self.today_filter.insert(feature)
         self.history_filter.insert(feature)
 
-    def exists(self, feature):
+    # 不建议生产使用， only for test
+    def _exists(self, feature):
         self.today_filter.exists(feature) or self.history_filter.exists(feature)
 
 
 if __name__ == '__main__':
     s = Scheme()
-    s.insert("content")  # so far so good, finish log on next step... 2020/7/19
+    s._insert("content")
+    s.transfer()
+    # so far so good, finish log on next step... 2020/7/19
